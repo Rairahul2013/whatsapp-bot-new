66ddde6c5b010ba985c170effbf8a46f0a0b7c5a
Rairahul2013 <98821630+Rairahul2013@users.noreply.github.com>
2026-06-18 19:55:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function startBot() {
 
     sock.ev.on('messages.upsert', async (m) => {
         const msg = m.messages[0];
-        if (!msg.message || msg.key.fromMe) return;
+        if (!msg  !msg.message  msg.key.fromMe) return;
 
         const remoteJid = msg.key.remoteJid;
         const textMessage = msg.message?.conversation  msg.message?.extendedTextMessage?.text  "";
