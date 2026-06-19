7a5f1309c97a1766ef2a21fac807f557b593d345
Rairahul2013 <98821630+Rairahul2013@users.noreply.github.com>
2026-06-19 15:03:36 +0530

    Add global functions for dirname and require

--- global.js
+++ global.js
@@ -17,4 +17,59 @@ global.__filename = function filename(pathURL = import.meta.url, rmPrefix = plat
       : pathURL
     : pathToFileURL(pathURL).toString()
 }
+global.__dirname = function dirname(pathURL) {
+  return path.dirname(global.__filename(pathURL, true))
+}
+global.__require = function require(dir = import.meta.url) {
+  return createRequire(dir)
+}
+
+import chalk from 'chalk'
+import { spawn } from 'child_process'
+import lodash from 'lodash'
+import { JSONFile, Low } from 'lowdb'
+import NodeCache from 'node-cache'
+import { default as Pino, default as pino } from 'pino'
+import syntaxerror from 'syntax-error'
+import { format } from 'util'
+import yargs from 'yargs'
+import CloudDBAdapter from './src/lib/cloudDBAdapter.js'
+import { mongoDB, mongoDBV2 } from './src/lib/mongoDB.js'
+import { makeWASocket, protoType, serialize } from './src/lib/simple.js'
+
+const {
+  DisconnectReason,
+  useMultiFileAuthState,
+  MessageRetryMap,
+  fetchLatestWaWebVersion,
+  makeCacheableSignalKeyStore,
+  makeInMemoryStore,
+  Browsers,
+  proto,
+  delay,
+  jidNormalizedUser,
+} = await (
+  await import('@whiskeysockets/baileys')
+).default
+
+import readline from 'readline'
+
+dotenv.config()
+
+async function main() {
+  const txt = global.SESSION_ID
+  if (!txt) {
+    console.error('SESSION ID not found.')
+    return
+  }
+  try {
+    await SaveCreds(txt)
+    console.log('Check Completed.')
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+main()
+await delay(1000 * 10)
 // Baaki ka code waisa hi rahega...
+  
